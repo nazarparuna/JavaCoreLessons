6ec216d13714cfc0ba7f87b7aa92b62fc3fc76cc
Nazar Paruna <nazarparuna@gmail.com>
2019-10-23 18:14:51 +0300

    update lesson 5

--- src/lesson5/Lesson5.java
+++ src/lesson5/Lesson5.java
@@ -2,6 +2,58 @@ package lesson5;
 
 public class Lesson5 {
     public static void main(String[] args) {
-        
+
+        // clone проекту
+
+        // додати колаборантів
+
+        // створити об'єкти типу Object
+
+        // getClass()
+
+        // hashCode()
+
+        // toString()
+
+        // equals з String
+
+        // == з String
+
+        // String створені без new
+
+        // додати клас Shape, Circle, Triangle, Square
+
+        // override метод
+
+        // overload метод
+
+        // final змінна
+
+        // final метод
+
+        // final класс
+
+        // static  змінна
+
+        // static  метод
+
+        // абстрактний клас
+
+        // абстрактний метод
+
+        // String format()
+
+        // string
+
+        // numbers
+
+        // форматування цифр
+
+        // exponential
+
+        // hexadecimal
+
+        // Дата і час
+
     }
 }
