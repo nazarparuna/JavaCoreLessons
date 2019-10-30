bbcf73f6db89d50413cb92937091949880850585
Pac-man <nazarparuna@gmail.com>
2019-10-30 09:18:46 +0200

    add lesson 8

---
+++ src/lesson8/Main.java
@@ -0,0 +1,31 @@
+package lesson8;
+
+public class Main {
+    public static void main(String[] args) {
+        // створення enum класу День
+
+        // доступ до елементів класу
+
+        // enum всередині класу
+
+        // enum в switch
+
+        // цикл по values
+
+        // name
+
+        // ordinal
+
+        // getDeclaringClass
+
+        // valueOf
+
+        //Scanner
+
+        //BufferedReader
+
+        //
+
+
+    }
+}
