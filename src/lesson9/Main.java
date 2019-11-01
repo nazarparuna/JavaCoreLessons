14493403fb1141072354010d54b290c1b25e9813
Nazar Paruna <nazarparuna@gmail.com>
2019-11-01 18:27:05 +0200

    add lesson 9

---
+++ src/lesson9/Main.java
@@ -0,0 +1,53 @@
+package lesson9;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // приклади null, indexoutof
+
+        // create exeption
+
+        // init exception
+
+        // throw exception
+
+        // re-throw exception
+
+        // catch exception
+
+        // catch multiple
+
+        // finally
+
+        // try-finally
+
+        // недохід до блоку finally
+
+        // try-with-resources
+
+        // autoclosable
+
+        /*
+            Exceptions
+         */
+
+        // створити клас банківський рахунок
+
+        // додати методи
+
+        // викинути помилку
+
+        /*
+        Documentation
+         */
+
+        // заголовок
+
+        // since, author, version
+
+        // змінні, конструктори
+
+        // методи (param, exeption, author, return, see)
+
+    }
+}
