c8d521088a2be595c9657b4374a1beb820b3cb94
Nazar Paruna <nazarparuna@gmail.com>
2019-11-06 14:42:13 +0200

    add lesson 11

---
+++ src/lesson11/Main.java
@@ -0,0 +1,42 @@
+package lesson11;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // overview methods of iterable
+
+        // overview methods of collections
+
+        // asList
+
+        // sort
+
+        // sort with indexes
+
+        // sort with Comparator
+
+        // binarySearch
+        // compare with linear search
+
+        // copyOf
+
+        // copyOfRange
+
+        // deepEquals, == , equals
+
+        // toString
+
+        //deepToString()
+
+        // empty array
+        // initialized array
+        // two-dimensional array
+        // array with Persons
+
+        // deepHashcode
+
+        // Arrays.fill() -  all types include String
+
+    }
+
+}
