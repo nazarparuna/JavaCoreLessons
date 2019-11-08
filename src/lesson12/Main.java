9b2b62c6a0207f903427fac572ac2483e92a23b1
Pac-man <nazarparuna@gmail.com>
2019-11-08 18:42:55 +0200

    add lesson 12

---
+++ src/lesson12/Main.java
@@ -0,0 +1,49 @@
+package lesson12;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // методи ліста
+
+        // методи ArrayList
+
+        // size
+
+        // вставка
+
+        // addAll
+
+        // clear
+
+        // contains
+
+        // equals
+
+        // remove
+
+        // get
+
+        // get поза межами
+
+        // створення з масиву
+
+        // subList
+
+        /*
+        Linked List
+         */
+
+        // add
+
+        // get
+
+        // addFirst
+
+        // addLast
+
+        // offer
+
+        // poll / pollLast
+
+    }
+}
