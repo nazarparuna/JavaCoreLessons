24ea94ecf423e23f322bdaafa061499e35c1eea8
Pac-man <nazarparuna@gmail.com>
2019-12-02 18:30:49 +0200

    update lesson 21

---
+++ src/lesson21/AppDates.java
@@ -0,0 +1,30 @@
+package lesson21;
+
+public class AppDates {
+    public static void main(String[] args) {
+
+        // local date
+
+        // plus time
+
+        // getEra
+
+        // is leap year
+
+        // start of day
+
+        // of epoch time
+
+        // create local time
+
+        // plus time
+
+        // trunkated to
+
+        // create local date time
+
+        // plus
+
+
+    }
+}
---
+++ src/lesson21/ApplicationAnnotation.java
@@ -0,0 +1,22 @@
+package lesson21;
+
+public class ApplicationAnnotation {
+    public static void main(String[] args) {
+
+        // create Droid class
+
+        // mark field with annotation
+
+        // get annotation values
+
+        // get not annotation values
+
+
+        // create class Student
+
+        // create new annotation
+
+        // get annotated fields and values
+    }
+
+}
