919d09760c0e6c01f346d68316982d63c9decffb
Nazar Paruna <nazarparuna@gmail.com>
2020-09-30 14:35:22 +0300

    add lesson 3

---
+++ src/lesson3/Main.java
@@ -0,0 +1,16 @@
+package lesson3;
+
+public class Main {
+    public static void main(String[] args) {
+        // create class Human
+        // add few fields in class Human
+        // create create variable of class Human
+        // assign object to this variable
+        // show output of this object
+        // add other constructors to this class
+        // repeat creation with other parameters and constructors
+        // make fields private
+        // add setters and getters
+
+    }
+}
