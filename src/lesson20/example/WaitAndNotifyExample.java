8359c304e779c978b5f5f047e0c2d2a84ef60b44
Nazar Paruna <nazar.m.paruna@globallogic.com>
2020-11-13 23:22:14 +0200

    updated wait/notify example

--- src/lesson20/example/WaitAndNotifyExample.java
+++ src/lesson20/example/WaitAndNotifyExample.java
@@ -2,23 +2,25 @@ package lesson20.example;
 
 public class WaitAndNotifyExample {
 
-    public static int counter = 0;
-
     public static void main(String[] args) {
 
+        // спільний монітор для синхрохізації роботи потоків
         WaitAndNotifyExample waitAndNotifyExample = new WaitAndNotifyExample();
 
         class Thread1 extends Thread {
             @Override
             public void run() {
-                System.out.println("Before wait method....");
-                WaitAndNotifyExample.increment();
-                try {
-                    waitAndNotifyExample.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                synchronized (waitAndNotifyExample) {
+                    System.out.println("Before wait method....");
+                    try {
+                        // виклик wait() на спільному моніторі передає розблоковує монітор
+                        // і дозволяє іншому потоку працювати з ним
+                        waitAndNotifyExample.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("After returning from thread #2...");
                 }
-                System.out.println("After returning from thread #2...");
             }
         }
 
@@ -26,15 +28,22 @@ public class WaitAndNotifyExample {
             @Override
             public void run() {
                 try {
+                    // додано таймер, щоб потім №2 розпочав свою роботу пізніше
+                    // (а відповідно потік №1 залочився на моніторі першим)
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                System.out.println("Inside another thread....");
-                for (int i = 0; i < 100; i++) {
-                    WaitAndNotifyExample.increment();
+                synchronized (waitAndNotifyExample) {
+                    System.out.println("Inside another thread....");
+                    for (int i = 0; i < 10; i++) {
+                        System.out.print(i + " ");
+                    }
+                    System.out.println();
+
+                    // виклик notify() на спільному моніторі повертає роботу в 1-ий потік
+                    waitAndNotifyExample.notify();
                 }
-                waitAndNotifyExample.notify();
             }
         }
 
@@ -44,8 +53,5 @@ public class WaitAndNotifyExample {
         thread2.start();
     }
 
-    public synchronized static void increment() {
-        counter++;
-    }
 }
 
